2e777b0b1d21cdebca1dfed11792940954f7181c
Brandon Held <wildwestcowboy12@gmail.com>
2020-07-14 18:54:49 -0500

    Working route

--- app.js
+++ app.js
@@ -2,13 +2,9 @@ const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
-const csrf = require('csurf');
 const { environment } = require("./config");
 const landing = require('./routes/landing');
 const app = express();
-const csrfProtection = csrf({ cookie: true });
-const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
-
 app.set('view engine', 'pug');
 app.use(morgan('dev'));
 app.use(cookieParser());
@@ -16,14 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 
 
 
-
-
-
-
 app.get('/', landing)
 
 
-
-
-
-module.exports = { app, asyncHandler };
+module.exports = app;
--- routes/landing.js
+++ routes/landing.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const { asyncHandler } = require("../app");
+const { asyncHandler } = require("../utils");
 const router = express.Router();
-// const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
 router.get('/', asyncHandler(async (req, res) => {
     res.render("landingPage");
   })
 );
-// router.get('/', (req, res) => {
-//   res.render("landingPage")
-// })
+
 module.exports = router;
