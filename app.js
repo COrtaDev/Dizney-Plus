36664fc52a9a99767b46ef37441912870e030175
Brandon Held <wildwestcowboy12@gmail.com>
2020-07-14 16:47:26 -0500

    ordered const

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const csrf = require('csurf');
 const { environment } = require("./config");
-
 const app = express();
 const csrfProtection = csrf({ cookie: true });
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
 app.set('view engine', 'pug');
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
 
 
 
