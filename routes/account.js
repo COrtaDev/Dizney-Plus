45eaeb2a1ec32d9e1894cf573c36eb8259bc7ed9
kimd345 <dong_hyuk_kim@alumni.brown.edu>
2020-07-15 17:20:20 -0400

    Made post route for account

--- routes/account.js
+++ routes/account.js
@@ -11,36 +11,66 @@ const router = express.Router();
 
 router.get('/account/sign-up', csrfProtection, (req, res) => {
     const account = db.Account.build();
-    res.render('sign-up', {
-        title: 'STEP 1 OF 3',
+    res.render('account-sign-up', {
+        title: 'Sign up',
         account,
         csrfToken: req.csrfToken(),
     });
 });
 
 const accountValidators = [
-  check("email")
-    .exists({ checkFalsy: true })
-    .withMessage("Please enter a value for Email")
-    // .isLength({ max: 255 })
-    // .withMessage('Email must not be more than 255 characters long'),
-    .isEmail()
-    .withMessage("Email is not valid")
-    .custom((value) => {
-      return db.Account.findOne({ where: { email: value } }).then((account) => {
-        if (account) {
-          return Promise.reject("The Email is already in use");
-        }
-      });
+    check("email")
+        .exists({ checkFalsy: true })
+        .withMessage("Please enter a value for Email")
+        // .isLength({ max: 255 })
+        // .withMessage('Email must not be more than 255 characters long'),
+        .isEmail()
+        .withMessage("Email is not valid")
+        .custom((value) => {
+            return db.Account.findOne({ where: { email: value } }).then((account) => {
+                if (account) {
+                    return Promise.reject("The Email is already in use");
+                }
+            });
     }),
-  check("password")
-    .exists({ checkFalsy: true })
-    .withMessage("Please enter a value for Password")
-    .isLength({ max: 50 })
-    .withMessage("Password must not be more than 50 characters long")
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, "g")
-    .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+    check("password")
+        .exists({ checkFalsy: true })
+        .withMessage("Please enter a value for Password")
+        // .isLength({ max: 50 })
+        // .withMessage("Password must not be more than 50 characters long")
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, "g")
+        .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
 ];
 
+router.post('/account/sign-up', csrfProtection, accountValidators,
+    asyncHandler(async (req, res) => {
+        const {
+            email,
+            password,
+        } = req.body;
+
+        const account = db.Account.build({
+            email
+        });
+
+        const validatorErrors = validationResult(req);
+
+        if (validatorErrors.isEmpty()) {
+            const passwordDigest = await bcrypt.hash(password, 10);
+            account.passwordDigest = passwordDigest;
+            await account.save();
+            // loginAccount(req, res, login);
+            res.redirect('/');
+        } else {
+            const errors = validatorErrors.array().map((error) => error.msg);
+            res.render('account-sign-up', {
+                title: 'Sign up',
+                account,
+                errors,
+                csrfToken: req.csrfToken(),
+            });
+        }
+    })
+);
 
 module.exports = router;
