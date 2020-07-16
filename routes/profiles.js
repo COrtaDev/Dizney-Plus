791cf8f81b7b8cfe8926a552d3da50a252f652e2
Conrad Orta <corta.dev@gmail.com>
2020-07-16 15:56:27 -0400

    Structed Profiles

--- routes/profiles.js
+++ routes/profiles.js
@@ -3,29 +3,37 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const db = require('../db/models');
 const bcrypt = require('bcryptjs');
+// const multer  = require('multer');//may be optional
 
 const { check, validationResult } = require('express-validator')
 const { csrfProtection, asyncHandler } = require('../utils');
 const { access_id, secret } = require('../config');
 
 const router = express.Router();
-s3.listBuckets(function(err, data) { console.log(err, data); });
+s3.listBuckets(function (err, data) { console.log(err, data); });
 
-// const multer  = require('multer');//may be optional
 
-function retrieveFile(filename,res){
+router.get('/profiles/select-avatar', (req, res) => {
+  res.render('profiles-select-avatar')
+})
 
-    const getParams = {
-      Bucket: 'sample-bucket-name',
-      Key: filename
-    };
+router.get('/profiles/select-profile', (req, res) => {
+  res.render('profiles-select-profile')
+})
 
-    s3.getObject(getParams, function(err, data) {
-      if (err){
-        return res.status(400).send({success:false,err:err});
-      }
-      else{
-        return res.send(data.Body);
-      }
-    });
-  }
+function retrieveFile(filename, res) {
+
+  const getParams = {
+    Bucket: 'sample-bucket-name',
+    Key: filename
+  };
+
+  s3.getObject(getParams, function (err, data) {
+    if (err) {
+      return res.status(400).send({ success: false, err: err });
+    }
+    else {
+      return res.send(data.Body);
+    }
+  });
+}
