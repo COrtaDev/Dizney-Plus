8a3466b2d93acc75cfb42bf479d0856f4f6b3c43
Brandon Held <wildwestcowboy12@gmail.com>
2020-07-17 11:31:54 -0500

    added movie route

--- routes/movieTab.js
+++ routes/movieTab.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const { asyncHandler } = require("../utils");
 const router = express.Router();
-
+const { Videos} = require('../db/models')
 router.get('/movies', asyncHandler(async (req, res) => {
-    res.render("movieTab");
+    const videos = Videos.findAll();
+    res.render("movieTab", {videos});
   })
 );
 
 router.get('/movies/:genre', asyncHandler(async (req, res) => {
     console.log(req.params.genre)
+
 }))
 module.exports = router;
