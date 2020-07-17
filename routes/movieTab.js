e4b1789fce409d88fdddd97a53925f4735b293c7
Brandon Held <wildwestcowboy12@gmail.com>
2020-07-17 09:53:44 -0500

    drop down menu working

--- routes/movieTab.js
+++ routes/movieTab.js
@@ -2,9 +2,12 @@ const express = require('express');
 const { asyncHandler } = require("../utils");
 const router = express.Router();
 
-router.get('/', asyncHandler(async (req, res) => {
+router.get('/movies', asyncHandler(async (req, res) => {
     res.render("movieTab");
   })
 );
 
+router.get('/movies/:genre', asyncHandler(async (req, res) => {
+    console.log(req.params.genre)
+}))
 module.exports = router;
