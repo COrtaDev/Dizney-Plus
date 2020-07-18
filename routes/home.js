7d3d2dd6e7ef4d52125137dd86bb9b6761e30e48
Robert M. Estrada <57651772+robertestrada@users.noreply.github.com>
2020-07-18 13:43:23 -0700

    Add home page links

--- routes/home.js
+++ routes/home.js
@@ -3,15 +3,17 @@ const router = express.Router();
 const { asyncHandler } = require("../utils");
 const { requireAuth } = require('../auth');
 const { Profile } = require('../db/models');
+const { Video } = require('../db/models');
 
 router.get("/home",
-  // requireAuth,
+  requireAuth,
   asyncHandler(async (req, res) => {
   const profiles = await Profile.findAll({
     where: {
-      // accountId: req.session.auth.accountId
+      accountId: req.session.auth.accountId
     }
   });
+  const videos = await Video.findAll({ });
   res.render("home", { profiles });
 }));
 
