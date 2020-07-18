1c305f26c52acb9326a5e00bb84858360931b70b
Conrad Orta <corta.dev@gmail.com>
2020-07-18 14:00:44 -0400

    Modified site assests

--- bin/banner-export.js
+++ bin/banner-export.js
@@ -4,38 +4,46 @@
     {
         "siteAssets": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/1000px-Dizney+_logo.svg.png",
         "bannerBackgrounds": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/backgrounds/1_avengers infinity war_4.jpeg",
-        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/1_avengers infinity war_5.png"
+        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/1_avengers infinity war_5.png",
+        "ratings": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/ratings/movie-g.png"
     },
     {
         "siteAssets": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/background.png",
         "bannerBackgrounds": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/backgrounds/2_solo a star wars story_4.jpeg",
-        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/2_solo a star wars story_5.png"
+        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/2_solo a star wars story_5.png",
+        "ratings": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/ratings/movie-pg.png"
     },
     {
         "siteAssets": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/favicon.ico",
         "bannerBackgrounds": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/backgrounds/3_x-men days of future past_4.jpeg",
-        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/3_x-men days of future past_5.png"
+        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/3_x-men days of future past_5.png",
+        "ratings": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/ratings/movie-pg-13.png"
     },
     {
         "siteAssets": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/log-in-screen-background.jpg",
         "bannerBackgrounds": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/backgrounds/4_hamilton_4.jpeg",
-        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/4_hamilton_5.png"
+        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/4_hamilton_5.png",
+        "ratings": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/ratings/tv-14.png"
     },
     {
         "bannerBackgrounds": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/backgrounds/5_cosmos_4.jpeg",
-        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/5_cosmos_5.png"
+        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/5_cosmos_5.png",
+        "ratings": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/ratings/tv-g.png"
     },
     {
         "bannerBackgrounds": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/backgrounds/6_percy jackson_4.jpeg",
-        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/6_percy jackson_5.png"
+        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/6_percy jackson_5.png",
+        "ratings": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/ratings/tv-pg.png"
     },
     {
         "bannerBackgrounds": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/backgrounds/7_the mighty ducks_4.jpeg",
-        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/7_the mighty ducks_5.png"
+        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/7_the mighty ducks_5.png",
+        "ratings": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/ratings/tv-y.png"
     },
     {
         "bannerBackgrounds": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/backgrounds/8_the simpsons movie_4.jpeg",
-        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/8_the simpsons movie_5.png"
+        "bannerTitles": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/titles/8_the simpsons movie_5.png",
+        "ratings": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/ratings/tv-y7.png"
     },
     {
         "bannerBackgrounds": "https://dizneyplus.s3.us-east-2.amazonaws.com/images/disneyPlusRips/banners/backgrounds/9_artemis fowl_4.jpeg",
