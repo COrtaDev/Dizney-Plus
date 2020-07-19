8f398925c8fdc9e65863cf2b44bdbbab151563ac
Robert M. Estrada <57651772+robertestrada@users.noreply.github.com>
2020-07-19 09:54:36 -0700

    Work on nav bar

---
+++ public/scripts/homeNavButtons.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const routes = document.querySelectorAll('[data-route]');
+    routes.forEach(div => {
+        div.addEventListener('click', e => {
+            e.stopPropagation()
+            const route = e.currentTarget.getAttribute('data-route');
+            // window.location.href = `/${route}`;
+            const { profiles } = req.body;
+            res.render('movieTab', { profiles, profile1 });
+        });
+    });
+
+    // const dropdown = document.querySelectorAll('.nav__dropdown');
+    // dropdown.addEventListener('click', e => {
+    //     e.stopPropagation();
+    //     const route = e.currentTarget.getAttribute('data-route');
+    //     window.location.href = `/${route}`;
+    // })
+
+    const logoutButton = document.querySelector('.logoutButton')
+    logoutButton.addEventListener('click', e => {
+        e.stopPropagation();
+        window.location.href = "/"; 
+    })
+});
