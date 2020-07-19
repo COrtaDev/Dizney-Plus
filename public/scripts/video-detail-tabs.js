2aebe5dcfea1e3e15610643a3cbbc869d22672e4
kimd345 <dong_hyuk_kim@alumni.brown.edu>
2020-07-19 15:57:17 -0400

    Integrate nav-bar and fix play button

--- public/scripts/video-detail-tabs.js
+++ public/scripts/video-detail-tabs.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   const tabs = document.querySelectorAll(".bar .bar__tabs li");
   const sections = document.querySelectorAll(".bar .tabContent");
-
+  const playRoutes = document.querySelectorAll('[play-route]');
+  
   tabs.forEach((tab) => {
     tab.addEventListener("click", (e) => {
       e.preventDefault();
@@ -9,7 +10,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
       addActiveTab(tab);
     });
   });
-
+  
   const removeActiveTab = () => {
     tabs.forEach((tab) => {
       tab.classList.remove("bar__tabs--active");
@@ -25,4 +26,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const matchingSection = document.querySelector(href);
     matchingSection.classList.add("bar__tabs--active");
   };
+  
+  playRoutes.forEach(playRoute => {
+    playRoute.addEventListener('click', e => {
+      e.stopPropagation();
+      const route = e.currentTarget.getAttribute('play-route');
+      window.location.href = `/video/${route}/player`;
+    });
+  });
 });
