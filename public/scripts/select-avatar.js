cf3268dd8e10a6d01f23daef51a5616f98483237
Conrad Orta <corta.dev@gmail.com>
2020-07-20 07:53:48 -0400

    Add Profile'

--- public/scripts/select-avatar.js
+++ public/scripts/select-avatar.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
     let url = window.location.search.substring(1);
     const newAvatar = document.getElementById('newAvatar');
     newAvatar.src = url;
-    window.addEventListener('click', (e) => {
-        console.log(window.location.search)
-    })
+    // window.addEventListener('click', (e) => {
+    //     console.log(window.location.search)
+    // })
 })
