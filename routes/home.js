f2cf2137de9898e5d6eda36df2333c15a7966cba
Robert M. Estrada <57651772+robertestrada@users.noreply.github.com>
2020-07-20 04:32:19 -0700

    Work on nav bar and dropdown

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,41 @@ const { asyncHandler } = require("../utils");
 const { requireAuth } = require('../auth');
 const { Profile, Video, Avatar, Sequelize } = require('../db/models');
 const op = Sequelize.Op;
-// const { logoutAccount } = require('../auth');
+
+
+router.get("/home/:id", 
+  requireAuth,
+  asyncHandler(async (req, res) => {
+    const url = req.originalUrl
+    const idx = url.lastIndexOf("/");
+    const newId = parseInt(url.substring(idx + 1));
+
+    req.session.auth = {
+      accountId: req.session.auth.accountId,
+      whosWatching: newId
+    };
+
+    const profile1 = await Profile.findOne({
+      where: {
+        id: req.session.auth.whosWatching
+      },
+      include: Avatar
+    });
+
+    const profiles = await Profile.findAll({
+      where: {
+        [op.and]: [
+          { [op.not]: { id: req.session.auth.whosWatching } },
+          { accountId: req.session.auth.accountId }
+        ]
+      },
+      include: Avatar
+    });
+
+    res.render("home", { profiles, profile1 });
+  })
+);
+
 
 router.get("/home", requireAuth, 
   asyncHandler(async (req, res) => {
@@ -27,6 +61,7 @@ router.get("/home", requireAuth,
     });
 
   res.render("home", { profiles, profile1 });
-}));
+  })
+);
 
 module.exports = router;
